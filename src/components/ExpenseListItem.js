a9234e1b55a1aea93e7088770553a9ac2f73e1cc
Joyion Timmons <you@example.com>
2019-11-04 14:23:58 -0800

    add numeral.js and format currency

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom"
 import moment from "moment";
+import numeral from "numeral";
 
 
 const ExpenseListItem = ({dispatch, description, id, createdAt, amount}) => (
     <div>
         <Link to={"/edit/"+ id}>Edit</Link>
         <h1>{description}</h1>
-        <p>{amount} - {createdAt} <span> { moment(createdAt).format('MMMM Do, YYYY')} </span></p>
+        <p>{numeral(amount / 100).format("$0,0.00")} - <span> { moment(createdAt).format('MMMM Do, YYYY')} </span></p>
     </div>
 
 );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ import {NavLink} from 'react-router-dom';
 const Header = () => (
     <div>
        <h2>Expense App</h2>
-       <NavLink activeClassName="is-active"  exact={true} to="/">Home</NavLink>
-       <NavLink activeClassName="is-active" to="/create">Create</NavLink>
-       <NavLink activeClassName="is-active" to="/edit">Edit</NavLink>
+       <NavLink activeClassName="is-active"  exact={true} to="/">View Expenses</NavLink>
+       <NavLink activeClassName="is-active" to="/create">Create Expense</NavLink>
     </div>
    
 );
